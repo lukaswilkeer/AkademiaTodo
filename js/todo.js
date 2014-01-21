94c3129aaabff0242c5242c0bb0d8b34fba4991c
Lukas Wilkeer <lukasc4l@gmail.com>
2014-01-21 17:00:55 -0200

    Esqueleto do controller.

---
+++ js/todo.js
@@ -0,0 +1,10 @@
+function TodoCtrl($scope){
+    $scope.todos= [
+        {text:'Planejar o próximo estudo da kolaborativa.', done:true},
+        {text:'Organizar o Akademia php.', done:false}];
+
+    $scope.addTodo = function (){};
+    $scopde.remaing = function (){};
+    $scope.archive = function (){};
+
+}
