4a68aa7634cf1e174de6f901eeb6acfc56c47774
Lukas Wilkeer <lukasc4l@gmail.com>
2014-02-04 17:25:48 -0200

    Crud controller.

---
+++ js/crud.js
@@ -0,0 +1,14 @@
+function CRUDCtrl($scope){
+
+    $scope.apitoken = "qCD4voLFRzCkQ2P6X4jAMdtqfGo34hL8CSyhKh3VV0";
+    $scope.apiurl = "https://api.mongohq.com/databases/";
+    $scope.database = "akademiaTodo-dev";
+
+
+    $scope.create = function(){
+
+    };
+    $scope.read = function(){};
+    $scope.update = function(){};
+    $scope.delet = function(){};
+}
